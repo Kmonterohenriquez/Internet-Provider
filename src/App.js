681cb96f70c52abde488ec75de5a859c80cc3746
kevin Montero <42505604+Kmonterohenriquez@users.noreply.github.com>
2019-09-03 09:32:19 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import NotFoundPage from './pages/NotFoundPage';
+import Home from './pages/Home';
+
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/404' component={NotFoundPage} />
+                    <Redirect to='/404'/>
+                </Switch>
+            </Router>       
+            )
+    }
+}
+
+export default App;
